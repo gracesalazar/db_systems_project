e8175d36ee4f1d3ea586d784e65d3ac3e0544cb9
grace <142949572+gracesalazar@users.noreply.github.com>
2024-04-29 21:44:36 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -8,14 +8,14 @@ CREATE TABLE albums (
 );
 
 CREATE TABLE artists (
-    genres TEXT, -- Assuming this can be stored as text
+    genres TEXT,
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255)
 );
 
 CREATE TABLE tracks (
-    album_id VARCHAR(255), -- Assuming this is related to an album ID
-    artists_id TEXT, -- Assuming this is a list of artist IDs and can be stored as text
+    album_id VARCHAR(255),
+    artists_id TEXT,
     duration_ms FLOAT,
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255),
@@ -23,51 +23,33 @@ CREATE TABLE tracks (
     popularity FLOAT
 );
 
--- one-to-one: track to album
-CREATE TABLE track_album (
-    track_id VARCHAR(255) PRIMARY KEY,
-    track_name TEXT,
-    track_duration FLOAT,
-    track_number VARCHAR(50),
-    album_id VARCHAR(255),
-    album_name TEXT,
-    album_release_date DATE,
-    album_total_tracks VARCHAR(50)
-);
+-- -- one-to-one: track to album
+-- CREATE TABLE track_album (
+--     track_id VARCHAR(255) PRIMARY KEY,
+--     track_name TEXT,
+--     track_duration FLOAT,
+--     track_number VARCHAR(50),
+--     album_id VARCHAR(255),
+--     album_name TEXT,
+--     album_release_date DATE,
+--     album_total_tracks VARCHAR(50)
+-- );
 
--- many-to-many: album to genre
-CREATE TABLE album_genre (
-    album_id VARCHAR(255),
-    album_name TEXT,
-    album_release_date DATE,
-    album_total_tracks VARCHAR(50),
-    genres TEXT
-);
+-- -- many-to-many: album to genre
+-- CREATE TABLE album_genre (
+--     album_id VARCHAR(255),
+--     album_name TEXT,
+--     album_release_date DATE,
+--     album_total_tracks VARCHAR(50),
+--     genres TEXT
+-- );
 
--- one-to-many: track to artist
-CREATE TABLE track_artist (
-    track_id VARCHAR(255) PRIMARY KEY,
-    track_name TEXT,
-    track_duration FLOAT,
-    track_number VARCHAR(50),
-    artist_id VARCHAR(255),
-    artist_name VARCHAR(255)
-);
-
--- all track info
-CREATE TABLE all_tracks (
-    track_name VARCHAR(255),
-    track_id VARCHAR(255) PRIMARY KEY,
-    track_number VARCHAR(50),
-    track_duration_ms FLOAT,
-    track_artists_id TEXT,
-    track_popularity FLOAT,
-    album_type VARCHAR(50),
-    album_id VARCHAR(255),
-    album_name VARCHAR(255),
-    album_release_date DATE,
-    album_total_tracks INT,
-    artist_genres TEXT,
-    artist_id VARCHAR(255),
-    artist_name VARCHAR(255)
-);
+-- -- one-to-many: track to artist
+-- CREATE TABLE track_artist (
+--     track_id VARCHAR(255) PRIMARY KEY,
+--     track_name TEXT,
+--     track_duration FLOAT,
+--     track_number VARCHAR(50),
+--     artist_id VARCHAR(255),
+--     artist_name VARCHAR(255)
+-- );
